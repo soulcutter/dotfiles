d7d74a06e1567f205994d91d7a0ea6a35cd8a7c2
Bradley Schaefer <bradley.schaefer@gmail.com>
2013-04-13 06:06:24 -0500

    Add git-pull-request
    Copied from https://github.com/splitbrain/git-pull-request/tree/python3
    @ 2ff003f35165dc40c32fb13295dd5f61a9b20e0a

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -18,11 +18,37 @@ aprc
 oh-my-zsh/custom
 "
 
+BIN="
+git-pull-request
+"
+
 # Create symbolic links for all configuration files
 for file in $FILES
 do
   SOURCE="$HOME/dotfiles/$file"
   TARGET="$HOME/.$file"
+
+  # Create backup file if the target already exists and is not a symlink
+  if [ -e "$TARGET" ] && [ ! -L "$TARGET" ]; then
+    echo "*** WARNING *** $TARGET already exists - copying original to .$file.old"
+    mv "$TARGET" "$TARGET.old"
+  fi
+
+  case $OSTYPE in
+    darwin*)
+      ln -hfsv "$SOURCE" "$TARGET"
+      ;;
+    linux*)
+      ln -fsv "$SOURCE" "$TARGET"
+      ;;
+  esac
+done
+
+# Create symbolic links for all bin files
+for file in $BIN
+do
+  SOURCE="$HOME/dotfiles/bin/$file"
+  TARGET="/usr/local/bin/$file"
 
   # Create backup file if the target already exists and is not a symlink
   if [ -e "$TARGET" ] && [ ! -L "$TARGET" ]; then
