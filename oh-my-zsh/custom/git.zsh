45778ee54f3522508027fd0e3a923b02c1f4a74d
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-07-14 17:24:09 -0500

    Add git=hub alias

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,3 +1,7 @@
 # change into git repo's top-level directory
 alias gcd='cd $(git rev-parse --show-toplevel)'
 
+# replace git command with hub https://hub.github.com/
+if [[ -n $(command -v hub) ]]; then
+  alias git=hub
+fi
