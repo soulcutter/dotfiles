ce8214a0a6f655e38fd96d1cb60b2ae14a54a1e6
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-07-16 16:12:12 -0500

    Add CIW aliases and remove migrate alias

--- oh-my-zsh/custom/tablexi.zsh
+++ oh-my-zsh/custom/tablexi.zsh
@@ -12,10 +12,21 @@ function grabtemplates {
   done
 }
 
+# SSH shortcuts 
 alias ssh-bella="ssh bellababy@bellababy.stage.tablexi.com"
 alias ssh-bella-uat="ssh bellababyuat@bellababyuat.stage.tablexi.com"
 alias ssh-flow="ssh uncommonflow@uncommon-flow.stage.tablexi.com"
 alias ssh-dickson="ssh viewmydata@viewmydata.stage.tablexi.com"
 alias ssh-push="ssh push@pushforwellness.stage.tablexi.com"
-alias migrate="rake db:migrate && rake db:test:prepare && annotate"
+
+# git related
+
+alias gmaster='git checkout master && git merge develop && git push origin master'
+
+# CIW
+alias ciw-prod-push='git push production master'
+alias ciw-prod-migrate='heroku run rake db:migrate --app chicagoideas'
+alias ciw-prod-restart='heroku restart --app chicagoideas'
+alias ciw-prod-sphinx='heroku run bundle exec flying-sphinx configure --app chicagoideas && heroku run bundle exec flying-sphinx rebuild --app chicagoideas'
+alias ciw-prod-deploy='ciw-prod-push && ciw-prod-migrate && ciw-prod-restart && ciw-prod-sphinx'
 
