a2cd1c3c1f32c6060a7bccbe0a78c96aae7668c1
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-08-29 17:14:43 -0500

    Add branchages alias to help with git cleanup

--- oh-my-zsh/custom/git.zsh
+++ oh-my-zsh/custom/git.zsh
@@ -1,6 +1,9 @@
 # change into git repo's top-level directory
 alias gcd='cd $(git rev-parse --show-toplevel)'
 
+# show age of branches
+alias branchages='for k in `git branch -r|sed -e"s/ ->.*//"|sed s/^..//`;do echo -e `git log -1 --pretty=format:"%Cgreen%ci %Cblue%cr%Creset" "$k" --`\\t"$k";done|sort -r'
+
 # replace git command with hub https://hub.github.com/
 if [[ -n $(command -v hub) ]]; then
   alias git=hub
