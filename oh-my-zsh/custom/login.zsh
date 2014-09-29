32a8b8e50ab552b7940963940479db21a3cb899f
Bradley Schaefer <bradley.schaefer@gmail.com>
2014-09-29 15:52:33 -0500

    Add more to default gitignore

--- oh-my-zsh/custom/login.zsh
+++ oh-my-zsh/custom/login.zsh
@@ -1,20 +1,20 @@
 
 function add-login() {
   if grep -qsE "^$1" .ssh-logins; then
-    sed -i "" -e "s/`grep -E "^$1 " .ssh-logins`/$*/" .ssh-logins
+    sed -i "" -e "s/`grep -E "^$1 " ~/.ssh-logins`/$*/" ~/.ssh-logins
   else
-    echo $* >> .ssh-logins
+    echo $* >> ~/.ssh-logins
   fi
 
   if [ -f .gitignore ]; then
-    if ! grep -qE ".ssh-logins" .gitignore; then
-      echo ".ssh-logins" >> .gitignore
+    if ! grep -qE "~/.ssh-logins" .gitignore; then
+      echo "~/.ssh-logins" >> .gitignore
     fi
   fi
 }
 
 function get-login() {
-  local access=( `grep -E "^$1 " .ssh-logins` )
+  local access=( `grep -E "^$1 " ~/.ssh-logins` )
   echo ${access[1]}
 }
 
