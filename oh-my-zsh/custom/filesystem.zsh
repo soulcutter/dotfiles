4f1c292ea1911f8c80aecbf32941df4e045858d6
Bradley Schaefer <bradley.schaefer@gmail.com>
2015-06-15 14:46:32 -0400

    Allow passing args to du in duh

--- oh-my-zsh/custom/filesystem.zsh
+++ oh-my-zsh/custom/filesystem.zsh
@@ -1,2 +1,5 @@
 # Human-readable disk usage sorted by size
-alias duh="du -h | awk '{if (\$1 != \"0B\" && \$1 != \"4.0K\") printf \"%s %08.2f\t%s\n\", index(\"KMG\", substr(\$1, length(\$1))), substr(\$1, 0, length(\$1)-1), \$0}' | sort -r | cut -f2,3 | less -FX"
+function duh() {
+  echo "$@"
+  du -h "$@" | awk '{if ($1 != "0B" && $1 != "4.0K") printf "%s %08.2f\t%s\n", index("KMG", substr($1, length($1))), substr($1, 0, length($1)-1), $0}' | sort -r | cut -f2,3 | less -FX
+}
