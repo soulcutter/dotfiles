bbf2a7d81b5d7be590e7aacc3ea8f0f5cb55c7b7
Bradley Schaefer <soulcutter@soulcutter.com>
2025-11-10 17:25:04 -0500

    Convert op_login from alias to function
    - Changed op_login from an alias to a proper function
    - Now calls op_login_github, op_login_npm, and op_login_datadog in succession

--- oh-my-zsh/custom/login.zsh
+++ oh-my-zsh/custom/login.zsh
@@ -1,7 +1,33 @@
+function op_login() {
+  op_login_github
+  op_login_npm
+  op_login_datadog
+}
 
-alias op_login='op_login_github && op_login_npm'
-alias op_login_github='export GITHUB_TOKEN=$(op read "op://Private/GitHub Personal Access Token/token") GH_TOKEN=$(op read "op://Private/GitHub Personal Access Token/token") GH_USERNAME=$(op read "op://Private/GitHub Personal Access Token/username")'
-alias op_login_npm='export NPM_TOKEN=$(op read "op://Private/Npmjs/token")'
+function op_login_github() {
+  # Export GitHub credentials
+  echo "[op_login_github] Setting up GitHub credentials from 1Password..."
+  export GITHUB_TOKEN=`op read "op://Private/GitHub Personal Access Token/token"`
+  export GH_TOKEN=`op read "op://Private/GitHub Personal Access Token/token"`
+  export GH_USERNAME=`op read "op://Private/GitHub Personal Access Token/username"`
+  echo "[op_login_github] Setup Complete."
+}
+
+function op_login_npm() {
+  # Export NPM token
+  echo "[op_login_npm] Setting up NPM credentials from 1Password..."
+  export NPM_TOKEN=`op read "op://Private/Npmjs/token"`
+  echo "[op_login_npm] Setup Complete."
+}
+
+function op_login_datadog() {
+  # Export Datadog API and application keys
+  echo "[op_login_datadog] Setting up DataDog API credentials from 1Password..."
+  export DD_APP_KEY=`op read "op://Private/Datadog/Application Key"`
+  export DD_API_KEY=`op read "op://Private/Datadog/Api Key"`
+  echo "[op_login_datadog] Setup Complete."
+}
+
 
 function add-login() {
   if grep -qsE "^$1" .ssh-logins; then
